993497386f58d298beedd92182ecb1bd3a710d12
Nick <ntorgov@gmail.com>
2017-03-01 13:53:16 +0300

    Simplify

--- test.ts
+++
@@ -1,7 +0,0 @@
-import {SimpleSchema} from 'meteor/aldeed:simple-schema';
-
-let schema1: SimpleSchema = new SimpleSchema({});
-
-let schema2 = new SimpleSchema([schema1, new SimpleSchema({})]);
-
-console.log(schema2.schema());
